89211b872ebcded3ca768e12a297d14cddd99165
sahil krishna cb <sahilkrishnacb@gmail.com>
2025-11-08 11:47:55 +0530

    makde it responsive

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './Login.css';
+import './login.css';
 import logo from '../../assets/logo.png'; 
 import axios from 'axios'
 import { useNavigate } from 'react-router-dom';
