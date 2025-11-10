348eab64b7bace72356625d86d3ebdaf10572626
sahil krishna cb <sahilkrishnacb@gmail.com>
2025-11-10 12:34:30 +0530

    all ddone

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,16 @@
-import React from 'react'
-import { StrictMode } from 'react'
+import React from "react";
+import { StrictMode } from "react";
 
-import './index.css'
-import {createRoot} from 'react-dom/client'
-import App from './App.jsx'
-import { CartProvider } from './components/UserContext.jsx'
+import "./index.css";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import { CartProvider } from "./components/UserContext.jsx";
 
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 const root = createRoot(container);
 
 root.render(
-
-    <CartProvider>
+  <CartProvider>
     <App />
-    </CartProvider>
+  </CartProvider>
 );
